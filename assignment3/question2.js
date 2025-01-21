b66710f278fefa17790658eccc638df002fc024c
jiahonglaw <law.jia.hong@gmail.com>
2025-01-22 06:32:34 +0800

    Added Questions 2 and 3

--- assignment3/question2.js
+++ assignment3/question2.js
@@ -1,3 +1,9 @@
 const msftData = [190.15, 194.21, 191.07, 190.44, 200.15];
 
 // Write your code below
+
+let msftDataNew = msftData;
+
+msftDataNew[5] = 205.67;
+
+console.log(msftDataNew)
--- assignment3/question3.js
+++ assignment3/question3.js
@@ -6,3 +6,19 @@ const disneyData = {
 };
 
 // Write your code below
+
+//convert all figures to float.
+
+let disneyOpen = parseFloat(disneyData["open"]);
+
+let disneyHigh = parseFloat(disneyData["high"]);
+
+let disneyLow = parseFloat(disneyData["low"]);
+
+let disneyClose = parseFloat(disneyData["close"]) ;
+
+let Result = disneyOpen - disneyHigh + disneyLow - disneyClose; //Calculate result
+
+Result = Result.toFixed(2); //Round the result to 2 decimals
+
+console.log(Result)
